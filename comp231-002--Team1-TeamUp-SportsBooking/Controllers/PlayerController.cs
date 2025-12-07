35418cefde69481e8b252690a74d0fa7597d5622
Dashkisss <Ids240605@gmail.com>
2025-12-07 16:57:25 -0500

    Updated layout and fixed navigation links

--- comp231-002--Team1-TeamUp-SportsBooking/Controllers/PlayerController.cs
+++ comp231-002--Team1-TeamUp-SportsBooking/Controllers/PlayerController.cs
@@ -1,68 +1,21 @@
-﻿using comp231_002__Team1_TeamUp_SportsBooking.Data;
-using comp231_002__Team1_TeamUp_SportsBooking.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace comp231_002__Team1_TeamUp_SportsBooking.Controllers
 {
     public class PlayerController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public PlayerController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         // GET: /Player
         public IActionResult Index()
         {
-            return View();   // sẽ cần Views/Player/Index.cshtml
+            return View();
         }
 
-        // GET: /Player/Create
+        // GET: /Player/Edit
+        // This is a frontend-only placeholder. Backend team will later bind real profile data.
         [HttpGet]
-        public async Task<IActionResult> Create()
+        public IActionResult Edit()
         {
-            ViewBag.Players = await _context.Players.ToListAsync();
-            ViewBag.Courts = await _context.Courts.ToListAsync();
-
-            var booking = new Booking
-            {
-                BookingDate = DateTime.Today
-            };
-
-            return View(booking);
-        }
-
-        // POST: /Player/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Booking booking)
-        {
-            if (!ModelState.IsValid)
-            {
-                ViewBag.Players = await _context.Players.ToListAsync();
-                ViewBag.Courts = await _context.Courts.ToListAsync();
-                return View(booking);
-            }
-
-            _context.Bookings.Add(booking);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index");
-        }
-
-        // ⭐ NEW: /Player/Bookings – My bookings
-        [HttpGet]
-        public async Task<IActionResult> Bookings()
-        {
-            // tạm thời lấy tất cả bookings, sau này m lọc theo PlayerID cũng được
-            var bookings = await _context.Bookings
-                .Include(b => b.Court)
-                .ToListAsync();
-
-            return View(bookings);   // dùng Views/Player/Bookings.cshtml
+            return View();
         }
     }
 }
